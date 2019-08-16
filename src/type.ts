910aab974bd8f8b942c75282c49f6e9fbb08fc82
Colin Luo <mail@luozhihua.com>
2019-08-16 16:49:41 +0800

    Add documentation;

--- src/type.ts
+++ src/type.ts
@@ -15,6 +15,32 @@ export type TreeNode<
       & TreeNodeWithChildren < KeyField, ChildrenField, NodeProps >
       & NodeProps;
 
+/**
+ * A function for matching nodes.
+ *
+ * @function nodesFinder
+ * @param {TreeNode} node
+ * @param {TreeNode[]} parents
+ * @returns {Boolean} return true if node is matched, or false if not matched;
+ */
 export type NodesFinder<Node> = (node: Node, parents: Node[]) => boolean;
+
+/**
+ * A function to sort node's children.
+ *
+ * @function nodeSorter
+ * @param {TreeNode} node
+ * @param {TreeNode[]} parents
+ * @returns {Boolean} return number [-1, 0, 1] for Array.sort;
+ */
 export type NodeSorter<Node> = (nodeA: Node, nodeB: Node, parents?: Node[]) => number;
+
+/**
+ * A function to map node.
+ *
+ * @function nodeMapper
+ * @param {TreeNode} node
+ * @param {TreeNode[]} parents
+ * @returns {TreeNode} return mapped node;
+ */
 export type NodeMapper<Node> = (node: Node, parents?: Node[]) => Node;
