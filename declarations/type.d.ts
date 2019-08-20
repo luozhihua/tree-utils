b60d6a089866b18fbe0719e722b965d042397949
Colin Luo <mail@luozhihua.com>
2019-08-20 12:58:53 +0800

    v3.0.1

--- declarations/type.d.ts
+++ declarations/type.d.ts
@@ -1,12 +1,12 @@
 declare type TreeNodeWithKey<K extends string, V = string> = {
     [P in K]: V;
 };
-declare type TreeNodeWithChildren<K extends string, C extends string, NodeProps> = {
+declare type TreeNodeWithChildren<NodeProps, K extends string, C extends string> = {
     [P in C]?: TreeNode<NodeProps, K, C>[];
 };
 export declare type TreeNode<NodeProps = {
     [k: string]: any;
-}, KeyField extends string = 'key', ChildrenField extends string = 'children'> = TreeNodeWithKey<KeyField> & TreeNodeWithChildren<KeyField, ChildrenField, NodeProps> & NodeProps;
+}, KeyField extends string = 'key', ChildrenField extends string = 'children'> = TreeNodeWithKey<KeyField> & TreeNodeWithChildren<NodeProps, KeyField, ChildrenField> & NodeProps;
 export declare type NodesFinder<Node> = (node: Node, parents: Node[]) => boolean;
 export declare type NodeSorter<Node> = (nodeA: Node, nodeB: Node, parents?: Node[]) => number;
 export declare type NodeMapper<Node> = (node: Node, parents?: Node[]) => Node;
--- dist/type.js
+++ dist/type.js
@@ -1,5 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function nodeSorter(nodeA, nodeB, parents) { return 0; }
-function nodeMapper(node, parents) { }
 //# sourceMappingURL=type.js.map
